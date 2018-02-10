7e1d771f48368dda9065809ec41a684ddca96e36
jonathan merandat <jonathan.merandat@outlook.com>
2018-02-10 20:00:55 +0100

    ajout vuisine

---
+++ js/commonFonction.js
@@ -0,0 +1,13 @@
+function buildElement(element, className, id, innerHTML, textContent, style) {
+    var elementHTML = document.createElement(element);
+    if (className != undefined) { elementHTML.className = className; }
+    if (id != undefined) { elementHTML.id = id; }
+    if (innerHTML != undefined) { elementHTML.innerHTML = innerHTML; }
+    if (textContent != undefined) { elementHTML.textContent = textContent; }
+    if (style != undefined) {
+        for (var i in style) {
+            elementHTML.style[i] = style[i];
+        }
+    }
+    return elementHTML;
+}
