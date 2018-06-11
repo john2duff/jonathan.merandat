b9555a1314dcba6af289e0fb3dd86aeffbdec835
jonathan merandat <jonathan.merandat@outlook.com>
2018-06-11 22:09:52 +0200

    Remise à jour

--- js/commonFonction.js
+++ js/commonFonction.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> daebf581245e4d79d7649d16fe96538521a90f21
 function buildElement(element, className, id, innerHTML, textContent, style) {
     var elementHTML = document.createElement(element);
     if (className != undefined) { elementHTML.className = className; }
@@ -34,14 +30,6 @@ var setSticky = function (evt) {
     var position = el.style.position;
     if (position != "sticky" || position == "") {
         el.style.position = "sticky";
-<<<<<<< HEAD
-        el.style.top = "0px";
-        evt.textContent = "Sticky on";
-    } else {
-        el.style.position = "initial";
-        el.style.position = "initial";
-        evt.textContent = "Sticky off";
-=======
         evt.style.backgroundColor = "orange";
         evt.style.opacity = "1";
         el.style.zIndex = "999999";
@@ -54,17 +42,12 @@ var setSticky = function (evt) {
         el.style.zIndex = "1";
         el.style.position = "initial";
         evt.textContent = "Epingler l'élement";
->>>>>>> daebf581245e4d79d7649d16fe96538521a90f21
     }
 }
 
 const CONTAINER = "container";
 const CONTAINER_FLUID = "container-fluid";
-<<<<<<< HEAD
-var modeAffichage = CONTAINER_FLUID;
-=======
 var modeAffichage = CONTAINER;
->>>>>>> daebf581245e4d79d7649d16fe96538521a90f21
 
 var actualiserModeAffichage = function () {
     var t = document.getElementsByClassName("modeAffichage");
